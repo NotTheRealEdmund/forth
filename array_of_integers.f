d5d6458469a934a7eb7cab24099b918de776b7bc
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-05-13 21:54:59 +0800

    Add array of integers

---
+++ array_of_integers.f
@@ -0,0 +1,12 @@
+VARIABLE array
+3 CELLS ALLOT
+: element  ( OFFSET -- ADDR ) CELLS array + ;
+10 1 element !
+200 2 element !
+3000 3 element !
+1 element ?
+2 element ?
+3 element ?
+1 element @
+2 element @
+3 element @
